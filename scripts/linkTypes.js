655702069e34bba4b8b1d24860751a8dd084d33b
Raunak Raj <71929976+bajrangCoder@users.noreply.github.com>
2026-01-17 17:36:33 +0530

    fix

--- scripts/linkTypes.js
+++ scripts/linkTypes.js
@@ -7,18 +7,18 @@ const BASE_DIR = resolve(__dirname, "../src");
  * @param {string} path
  */
 function linkTypes(path) {
-  const refs = []
-  for (const p of readdirSync(path)) {
-    if (statSync(join(path, p)).isDirectory()) {
-      linkTypes(join(path, p))
-      refs.push(`/// <reference path="./${p}/index.d.ts" />`)
-    } else {
-      if (p !== "index.d.ts" && p !== "test.ts") {
-        refs.push(`/// <reference path="./${p}" />`)
-      }
-    }
-  }
-  writeFileSync(join(path, "index.d.ts"), refs.join("\n"))
+	const refs = [];
+	for (const p of readdirSync(path)) {
+		if (statSync(join(path, p)).isDirectory()) {
+			linkTypes(join(path, p));
+			refs.push(`/// <reference path="./${p}/index.d.ts" />`);
+		} else {
+			if (p !== "index.d.ts" && p !== "test.ts") {
+				refs.push(`/// <reference path="./${p}" />`);
+			}
+		}
+	}
+	writeFileSync(join(path, "index.d.ts"), refs.join("\n"));
 }
 
-linkTypes(BASE_DIR)
+linkTypes(BASE_DIR);
